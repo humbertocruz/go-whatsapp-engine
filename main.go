e0aa6032618cdd48357b95fff44661fcc7cb236a
Humberto Cruz <humberto.cruz@gmail.com>
2026-02-11 16:31:12 -0300

    fix: identify device store by pharmacy id to avoid session conflicts

--- main.go
+++ main.go
@@ -133,7 +133,7 @@ func startAPI() {
 		}
 
 		// Tenta pegar dispositivo existente ou cria novo
-		deviceStore, err := container.GetFirstDevice(context.Background()) 
+		deviceStore, err := container.GetDevice(types.JID{User: id, Server: types.DefaultUserServer})
 		if err != nil || deviceStore == nil {
 			deviceStore = container.NewDevice()
 		}
